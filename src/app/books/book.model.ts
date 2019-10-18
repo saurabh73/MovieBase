9cc24523effb2125ae89f5abcaf3d08058d7eabf
Viktoria Jechsmayr <viktoria@jechsmayr.at>
2019-10-18 09:40:07 +0200

    book model and service added

---
+++ src/app/books/book.model.ts
@@ -0,0 +1,12 @@
+export interface Book{
+    id: number,
+    title: String,
+    author: String/*,
+    genre: String,
+    previous_in_series: number,
+    next_in_series: number,
+    language: String,
+    sites: number,
+    iban: String,
+    rating: number*/
+}
